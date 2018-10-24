f96e800c0d228034822bc8f420192fe4de1bb1ec
Rafid Hoda <rafidhoda@gmail.com>
2018-10-24 16:02:07 +0200

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+const title = React.createElement(
+    'h1',
+    { id: 'main-title', title: 'This is a title.' },
+    'Hello World'
+);
+
+ReactDOM.render(
+  title,document.getElementById('root')
+);
