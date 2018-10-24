8854b81846c98674de3c1437964ae8810a67dcad
Rafid Hoda <rafidhoda@gmail.com>
2018-10-24 16:33:47 +0200

    Change to JSX

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a title.' },
-    'Hello World'
-);
+const title = 'My First React Element!';
 
-const desc = React.createElement(
-  'p',
-  null,
-  'I just learned how to create a React node and render it into the DOM.'
-);
+const desc = 'I just learned how to create a React node and render it into the DOM.';
 
-const header = React.createElement(
-  'header',
-  null,
-  title,
-  desc
+const myTitleID = 'main-title';
+
+const name = 'Rafid';
+
+const header = (
+  <header>
+    <h1 id={myTitleID}>{ name }'s First React Element!</h1>
+    <p className="main-desc">{ desc }</p>
+  </header>
 );
 
 ReactDOM.render(
